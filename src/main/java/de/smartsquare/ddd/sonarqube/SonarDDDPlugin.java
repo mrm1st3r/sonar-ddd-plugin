bec959c69e80b1d85622930b02077d6a6536c886
Lukas Taake <lukas.taake@gmail.com>
2017-04-12 09:56:58 +0200

    Added empty entry point class

---
+++ src/main/java/de/smartsquare/ddd/sonarqube/SonarDDDPlugin.java
@@ -0,0 +1,13 @@
+package de.smartsquare.ddd.sonarqube;
+
+import org.sonar.api.Plugin;
+
+/**
+ * Entry point for DDD plugin.
+ */
+public class SonarDDDPlugin implements Plugin {
+
+    public void define(Context context) {
+
+    }
+}
