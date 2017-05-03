f18efacc4a1138f539c96e8a0779eda53c12e929
Lukas Taake <lukas.taake@gmail.com>
2017-05-03 10:21:46 +0200

    Added host parameter to deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+HOSTNAME=$1
+
 echo "Copying plugin..."
-scp target/sonar-ddd-plugin-0.0.1-SNAPSHOT.jar sonar.vm:/opt/sonar/extensions/plugins
+scp target/sonar-ddd-plugin-0.0.1-SNAPSHOT.jar $HOSTNAME:/opt/sonar/extensions/plugins
 echo "Restarting sonar..."
-ssh sonar.vm "sudo service sonar restart"
+ssh $HOSTNAME "sudo service sonar restart"
