2d6b915ebbf27e9258faf7a22b992bcc470a1328
Lukas Taake <lukas.taake@gmail.com>
2017-05-03 14:11:37 +0200

    Added tests for DDDSonarComponents

---
+++ src/test/groovy/de/smartsquare/ddd/sonarqube/DDDSonarComponentsTest.groovy
@@ -0,0 +1,41 @@
+package de.smartsquare.ddd.sonarqube
+
+import de.smartsquare.ddd.sonarqube.checks.ImmutabilityCheck
+import org.sonar.api.batch.fs.TextRange
+import org.sonar.api.rule.RuleKey
+import spock.lang.Specification
+
+class DDDSonarComponentsTest extends Specification {
+
+    def "should lookup RuleKey"() {
+        given:
+        def expectedKey = RuleKey.of(SonarDDDPlugin.REPOSITORY_KEY, "Immutability")
+        def check = new ImmutabilityCheck()
+        DDDSonarComponents components = createComponents()
+
+        when:
+        def key = components.getRuleKey(check)
+
+        then:
+        key == expectedKey
+    }
+
+    def "should mock newSymbol"() {
+        given:
+        def components = createComponents()
+
+        expect:
+        def symbolTable = components.symbolizableFor(new File(""))
+        symbolTable != null
+        symbolTable.save()
+        symbolTable.newSymbol(Mock(TextRange)) instanceof DDDSonarComponents.MockNewSymbol
+        symbolTable.newSymbol(0, 0) instanceof DDDSonarComponents.MockNewSymbol
+        symbolTable.newSymbol(0, 0, 0, 0) instanceof DDDSonarComponents.MockNewSymbol
+        symbolTable.newSymbol(Mock(TextRange)) instanceof DDDSonarComponents.MockNewSymbol
+    }
+
+    def createComponents() {
+        def components = new DDDSonarComponents(null, null, null, null, null)
+        components
+    }
+}
