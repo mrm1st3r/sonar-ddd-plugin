d681d8e7be9c65e57dfd27e6274a7b5d4c65a8c8
Lukas Taake <lukas.taake@gmail.com>
2017-05-03 11:20:24 +0200

    Added RuleKey lookup to DDDSonarComponents

--- src/main/java/de/smartsquare/ddd/sonarqube/DDDSonarComponents.java
+++ src/main/java/de/smartsquare/ddd/sonarqube/DDDSonarComponents.java
@@ -1,6 +1,5 @@
 package de.smartsquare.ddd.sonarqube;
 
-import org.sonar.api.batch.BatchSide;
 import org.sonar.api.batch.ScannerSide;
 import org.sonar.api.batch.fs.FileSystem;
 import org.sonar.api.batch.fs.InputFile;
@@ -9,21 +8,26 @@ import org.sonar.api.batch.rule.CheckFactory;
 import org.sonar.api.batch.sensor.symbol.NewSymbol;
 import org.sonar.api.batch.sensor.symbol.NewSymbolTable;
 import org.sonar.api.measures.FileLinesContextFactory;
+import org.sonar.api.rule.RuleKey;
+import org.sonar.check.Rule;
 import org.sonar.java.JavaClasspath;
 import org.sonar.java.JavaTestClasspath;
 import org.sonar.java.SonarComponents;
 import org.sonar.plugins.java.api.CheckRegistrar;
+import org.sonar.plugins.java.api.JavaCheck;
 import org.sonarsource.api.sonarlint.SonarLintSide;
 
 import javax.annotation.Nullable;
 import java.io.File;
 
 /**
- * Modified SonarComponents for
+ * Modified SonarComponents to fix double creation of symbol tables during DDD analysis.
+ * Also manages plugin-local RuleKey lookup
  */
 @ScannerSide
 @SonarLintSide
 public class DDDSonarComponents extends SonarComponents {
+
     public DDDSonarComponents(FileLinesContextFactory fileLinesContextFactory, FileSystem fs, JavaClasspath javaClasspath, JavaTestClasspath javaTestClasspath, CheckFactory checkFactory) {
         super(fileLinesContextFactory, fs, javaClasspath, javaTestClasspath, checkFactory);
     }
@@ -33,11 +37,17 @@ public class DDDSonarComponents extends SonarComponents {
     }
 
     @Override
+    public RuleKey getRuleKey(JavaCheck check) {
+        String ruleKey = check.getClass().getAnnotation(Rule.class).key();
+        return RuleKey.of(SonarDDDPlugin.REPOSITORY_KEY, ruleKey);
+    }
+
+    @Override
     public NewSymbolTable symbolizableFor(File file) {
         return new MockSymbolTable();
     }
 
-    public static class MockSymbolTable implements NewSymbolTable {
+    private static class MockSymbolTable implements NewSymbolTable {
 
         @Override
         public NewSymbolTable onFile(InputFile inputFile) {
@@ -56,11 +66,28 @@ public class DDDSonarComponents extends SonarComponents {
 
         @Override
         public NewSymbol newSymbol(int startLine, int startLineOffset, int endLine, int endLineOffset) {
-            return null;
+            return new MockNewSymbol();
         }
 
         @Override
         public void save() {
+        }
+
+    }
+    private static class MockNewSymbol implements NewSymbol {
+        @Override
+        public NewSymbol newReference(int startOffset, int endOffset) {
+            return null;
+        }
+
+        @Override
+        public NewSymbol newReference(TextRange range) {
+            return null;
+        }
+
+        @Override
+        public NewSymbol newReference(int startLine, int startLineOffset, int endLine, int endLineOffset) {
+            return null;
         }
     }
 }
