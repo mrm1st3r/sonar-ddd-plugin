a0b579a5ac8d964c4ec84b050549e8a0cec361e1
Lukas Taake <lukas.taake@gmail.com>
2017-05-10 15:34:34 +0200

    Fixed deploy script to use new module directory

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,6 @@ HOSTNAME=$1
 
 echo "Copying plugin..."
 ssh $HOSTNAME "rm /opt/sonar/extensions/plugins/sonar-ddd-plugin*.jar"
-scp target/sonar-ddd-plugin-*.jar $HOSTNAME:/opt/sonar/extensions/plugins
+scp ddd-plugin/target/sonar-ddd-plugin-*.jar $HOSTNAME:/opt/sonar/extensions/plugins
 echo "Restarting sonar..."
 ssh $HOSTNAME "sudo service sonar restart"
