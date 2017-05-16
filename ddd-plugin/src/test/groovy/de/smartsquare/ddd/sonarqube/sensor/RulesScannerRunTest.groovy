995119ee6e34ce1af2a94e33aae77d115fe098f1
Lukas Taake <lukas.taake@gmail.com>
2017-05-16 12:09:26 +0200

    Fixed tests

--- ddd-plugin/src/test/groovy/de/smartsquare/ddd/sonarqube/sensor/RulesScannerRunTest.groovy
+++ ddd-plugin/src/test/groovy/de/smartsquare/ddd/sonarqube/sensor/RulesScannerRunTest.groovy
@@ -2,6 +2,7 @@ package de.smartsquare.ddd.sonarqube.sensor
 
 import de.smartsquare.ddd.sonarqube.collect.ModelCollection
 import de.smartsquare.ddd.sonarqube.rules.RulesList
+import org.sonar.api.config.Settings
 import org.sonar.java.SonarComponents
 import org.sonar.java.checks.verifier.JavaCheckVerifier
 import org.sonar.plugins.java.api.JavaVersion
@@ -14,11 +15,13 @@ class RulesScannerRunTest extends Specification {
     def "should collect model classes"() {
         given:
         def model = Mock(ModelCollection)
+        def settings = Mock(Settings)
         def components = Mock(SonarComponents)
         def run = new RulesScannerRun(components,
                  JavaCheckVerifier.getFilesRecursively(Paths.get("target/test-jars"), ["jar"] as String[]),
                 Mock(JavaVersion),
-                model)
+                model,
+                settings)
 
         when:
         run.registerChecks(RulesList.checkClasses())
