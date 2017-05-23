ecff18c7b105d7f8482908893fd8e84d049ed32e
Lukas Taake <lukas.taake@gmail.com>
2017-05-23 14:01:52 +0200

    Added tests for RuleEnricher

---
+++ ddd-plugin/src/test/groovy/de/smartsquare/ddd/sonarqube/rules/RuleEnricherTest.groovy
@@ -0,0 +1,49 @@
+package de.smartsquare.ddd.sonarqube.rules
+
+import org.sonar.api.rule.RuleStatus
+import org.sonar.api.server.rule.RulesDefinition
+import spock.lang.Specification
+
+class RuleEnricherTest extends Specification {
+
+    RulesDefinition.NewRepository repository
+    String langKey
+    private RuleEnricher enricher
+    private RulesDefinition.NewRule rule
+
+    def setup() {
+        repository = Mock(RulesDefinition.NewRepository)
+        langKey = "java"
+        enricher = new RuleEnricher(repository, langKey)
+    }
+
+    def "should enrich rules"() {
+        given:
+        rule = new RulesDefinition.NewRule("ddd", "Immutability")
+
+        when:
+        enricher.enrich()
+
+        then:
+        repository.rules() >> [rule]
+        repository.key() >> "ddd"
+        rule.htmlDescription != null
+        rule.name == "Value objects should be immutable"
+        rule.status == RuleStatus.READY
+    }
+
+    def "should notice missing files"() {
+        given:
+        rule = new RulesDefinition.NewRule("ddd", "Missing")
+
+        when:
+        enricher.enrich()
+
+        then:
+        repository.rules() >> [rule]
+        repository.key() >> "ddd"
+        rule.name == null
+        rule.htmlDescription == null
+
+    }
+}
