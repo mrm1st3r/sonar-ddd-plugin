9df8aa0a454af8b8f6e262192119aff6cd3523c5
Lukas Taake <lukas.taake@gmail.com>
2017-09-18 19:04:50 +0200

    Include Value Objects in aggregate structure

--- ddd-plugin/src/main/java/de/smartsquare/ddd/sonarqube/collect/AggregateGraphBuilder.java
+++ ddd-plugin/src/main/java/de/smartsquare/ddd/sonarqube/collect/AggregateGraphBuilder.java
@@ -46,7 +46,7 @@ public class AggregateGraphBuilder extends IssuableSubscriptionVisitor {
                 .map(m -> ((VariableTree) m).symbol())
                 .filter(m -> !(m.isStatic() && m.isFinal()))
                 .map(m -> m.type().fullyQualifiedName())
-                .filter(modelCollection::hasEntity)
+                .filter(modelCollection::isAggregateRelevantType)
                 .forEach(member -> addAggregateRelation(className, member));
     }
 
